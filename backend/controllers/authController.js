0e787710851a52b603bbe3ed9ff45567b185ac2a
Randika <anurandika599@gmail.com>
2026-07-31 13:41:32 +0530

    Implement JWT authentication with secure login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,4 +1,5 @@
 const bcrypt = require("bcrypt");
+const jwt = require("jsonwebtoken");
 const { createUser, findUserByEmail } = require("../models/userModel");
 const register = async (req, res) => {
   try {
@@ -30,6 +31,65 @@ const register = async (req, res) => {
   }
 };
 
+    const login = async (req, res) => {
+    try {
+    const { email, password } = req.body;
+
+    const user = await findUserByEmail(email);
+    if (!user) {
+    return res.status(401).json({
+        success: false,
+        message: "Invalid email or password.",
+    });
+    }
+
+    const isPasswordMatch = await bcrypt.compare(password, user.password);
+    if (!isPasswordMatch) {
+    return res.status(401).json({
+        success: false,
+        message: "Invalid email or password.",
+    });
+    }
+
+    console.log("JWT_SECRET =", process.env.JWT_SECRET);
+
+    const token = jwt.sign(
+        {
+            id: user.id,
+            email: user.email,
+            role: user.role,
+        },
+        process.env.JWT_SECRET,
+        {
+            expiresIn: "1d",
+        }
+        );
+
+
+    res.status(200).json({
+        success: true,
+        message: "Login successful.",
+        token,
+        user: {
+            id: user.id,
+            full_name: user.full_name,
+            email: user.email,
+            role: user.role,
+        },
+    });
+
+    } catch (error) {
+        console.error("LOGIN ERROR:", error);
+
+        res.status(500).json({
+        success: false,
+        message: error.message,
+        });
+    }
+    };
+
+
 module.exports = {
   register,
+  login,
 };
